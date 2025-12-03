df2fed46c7e224922d307f7dddcb6630823da070
Hatim Hussain <hatimrockst@gmail.com>
2025-12-03 22:35:57 +0530

    f

--- Server/app.js
+++ Server/app.js
@@ -18,9 +18,10 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "../Client/dist")));
 
-app.get("/*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "../Client/dist/index.html"));
 });
+
 
 const server = http.createServer(app)
 
